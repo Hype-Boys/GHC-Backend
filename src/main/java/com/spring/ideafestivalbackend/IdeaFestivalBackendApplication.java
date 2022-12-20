a67da59f2e9033ebeb3d0b733d7d98fcc8603632
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-12-20 17:03:33 +0900

    first commit

---
+++ src/main/java/com/spring/ideafestivalbackend/IdeaFestivalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ideafestivalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaFestivalBackendApplication {
+    //test
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaFestivalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/ideafestivalbackend/IdeaFestivalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ideafestivalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaFestivalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

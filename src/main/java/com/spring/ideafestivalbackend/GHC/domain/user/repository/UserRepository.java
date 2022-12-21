ca1672fd1e50b0c1dac7f69b5d232352b32ef473
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-12-21 20:38:39 +0900

    회원 저장 기능

---
+++ src/main/java/com/spring/ideafestivalbackend/GHC/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.spring.ideafestivalbackend.GHC.domain.user.repository;
+
+import com.spring.ideafestivalbackend.GHC.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

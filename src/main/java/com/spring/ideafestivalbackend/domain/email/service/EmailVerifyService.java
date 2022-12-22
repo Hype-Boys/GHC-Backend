588f6592a066b9c3448e8bc974d841952e002517
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-12-22 09:19:01 +0900

    이메일 인증기능 구현

---
+++ src/main/java/com/spring/ideafestivalbackend/domain/email/service/EmailVerifyService.java
@@ -0,0 +1,26 @@
+package com.spring.ideafestivalbackend.domain.email.service;
+
+
+import com.spring.ideafestivalbackend.domain.email.entity.EmailEntity;
+import com.spring.ideafestivalbackend.domain.email.exception.MailAuthCodeExpiredException;
+import com.spring.ideafestivalbackend.domain.email.exception.NotMatchAuthCodeException;
+import com.spring.ideafestivalbackend.domain.email.repository.EmailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailVerifyService {
+    private EmailRepository emailVerifyRepository;
+
+    public void verificationMail(String email, String verifyCode){
+        EmailEntity emailVerifyEntity = emailVerifyRepository.findById(email)
+                .orElseThrow(()-> new MailAuthCodeExpiredException("인증코드가 만료되었습니다."));
+        if(emailVerifyEntity.getRandomValue() != verifyCode){
+            throw new NotMatchAuthCodeException("인증코드가 일치하지 않습니다.");
+        }
+        emailVerifyEntity.updateVerification(true);
+        emailVerifyRepository.save(emailVerifyEntity);
+    }
+
+}

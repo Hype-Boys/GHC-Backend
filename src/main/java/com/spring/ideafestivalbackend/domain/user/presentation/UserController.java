b815752818d046bbaa32b95f1abb06d285f8d711
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-12-26 23:29:12 +0900

    write user controller

--- src/main/java/com/spring/ideafestivalbackend/domain/user/presentation/UserController.java
+++ src/main/java/com/spring/ideafestivalbackend/domain/user/presentation/UserController.java
@@ -1,14 +1,24 @@
 package com.spring.ideafestivalbackend.domain.user.presentation;
 
 
+import com.spring.ideafestivalbackend.domain.user.presentation.dto.request.ChangePasswordRequest;
 import com.spring.ideafestivalbackend.domain.user.service.ChangePasswordService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("user")
 public class UserController {
     private final ChangePasswordService changePasswordService;
+
+    @PatchMapping
+    public ResponseEntity<Void> changePassword(@RequestBody @Valid ChangePasswordRequest changePasswordRequest){
+        changePasswordService.execute(changePasswordRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }

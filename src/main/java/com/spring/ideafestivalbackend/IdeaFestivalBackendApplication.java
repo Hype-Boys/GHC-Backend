b81fcc9a9f2954116000bfd735aa95d36cbeb64c
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-12-27 00:08:28 +0900

    CorsConfig

--- src/main/java/com/spring/ideafestivalbackend/IdeaFestivalBackendApplication.java
+++ src/main/java/com/spring/ideafestivalbackend/IdeaFestivalBackendApplication.java
@@ -3,7 +3,12 @@ package com.spring.ideafestivalbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class IdeaFestivalBackendApplication {
--- src/main/java/com/spring/ideafestivalbackend/global/webCofig/CorsConfig.java
+++ src/main/java/com/spring/ideafestivalbackend/global/webCofig/CorsConfig.java
@@ -9,5 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT", "HEAD")
+                .maxAge(3000);
     }
 }

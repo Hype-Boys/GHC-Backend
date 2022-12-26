75baa45ecff6c4fd034673b9ce803620ad854d56
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2022-12-27 00:29:31 +0900

    JwtRequestFilter

--- src/main/java/com/spring/ideafestivalbackend/global/filter/JwtRequestFilter.java
+++ src/main/java/com/spring/ideafestivalbackend/global/filter/JwtRequestFilter.java
@@ -1,9 +1,13 @@
 package com.spring.ideafestivalbackend.global.filter;
 
+import com.spring.ideafestivalbackend.domain.auth.exception.BlackListAlreadyExistException;
+import com.spring.ideafestivalbackend.global.exception.TokenNotValidException;
 import com.spring.ideafestivalbackend.global.security.jwt.properties.JwtProperties;
 import com.spring.ideafestivalbackend.global.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -23,6 +27,22 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("Authorization");
+        if(accessToken != null) {
+            tokenProvider.extractAllClaims(accessToken, jwtProperties.getAccessSecret());
+            System.out.printf(tokenProvider.getTokenType(accessToken,jwtProperties.getAccessSecret()));
+            if (!tokenProvider.getTokenType(accessToken, jwtProperties.getAccessSecret()).equals("ACCESS_TOKEN"))
+                throw new TokenNotValidException("Token is not valid");
+            else if(redisTemplate.opsForValue().get(accessToken)!=null)
+                throw new BlackListAlreadyExistException("블랙리스트에 이미 등록되어있습니다.");
+            String email = tokenProvider.getUserEmail(accessToken, jwtProperties.getAccessSecret());
+            registerSecurityContext(request, email);
+        }
+        filterChain.doFilter(request, response);
+    }
 
+    private void registerSecurityContext(HttpServletRequest request, String email) {
+        UsernamePasswordAuthenticationToken authenticationToken = tokenProvider.authentication(email);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
 }

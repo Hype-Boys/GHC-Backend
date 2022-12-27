91173761f3c29822306bafcf6cdd1a2932cd60f0
dlguswns3482 <103907181+dlguswns3482@users.noreply.github.com>
2022-12-27 18:22:34 +0900

    FILE DELETE

--- src/main/java/com/spring/ideafestivalbackend/domain/email/exception/repository/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.ideafestivalbackend.domain.email.exception.repository;
-
-import com.spring.ideafestivalbackend.domain.email.entity.EmailEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmailRepository extends CrudRepository<EmailEntity, String> {
-}
--- src/main/java/com/spring/ideafestivalbackend/domain/email/repository/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.ideafestivalbackend.domain.email.repository;
-
-import com.spring.ideafestivalbackend.domain.email.entity.EmailEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmailRepository extends CrudRepository<EmailEntity, String> {
-}
